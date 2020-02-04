60d310b135a192e54fd97b7e3b65affb864f3dec
LakhaniUsha <lakhaniusha11@gmail.com>
2020-02-04 14:40:41 -0500

    04-02(2)

---
+++ app/src/main/java/com/example/tool_sharing/Forget_psw.java
@@ -0,0 +1,14 @@
+package com.example.tool_sharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forget_psw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_psw);
+    }
+}
