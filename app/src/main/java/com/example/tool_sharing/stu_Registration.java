12ec217e425169f1330604f76cc0fb3f40710fe9
LakhaniUsha <lakhaniusha11@gmail.com>
2020-02-04 14:24:35 -0500

    04-02(1)

---
+++ app/src/main/java/com/example/tool_sharing/stu_Registration.java
@@ -0,0 +1,14 @@
+package com.example.tool_sharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stu_Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu__registration);
+    }
+}
